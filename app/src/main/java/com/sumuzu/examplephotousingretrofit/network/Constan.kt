3aede51cd943a98bcbb7077a9e2227c09aac1510
Ratana CS <54393025+RatanaCS@users.noreply.github.com>
2021-02-27 20:09:01 +0700

    update base url

--- app/src/main/java/com/sumuzu/examplephotousingretrofit/network/Constan.kt
+++ app/src/main/java/com/sumuzu/examplephotousingretrofit/network/Constan.kt
@@ -3,7 +3,7 @@ package com.sumuzu.examplephotousingretrofit.network
 class Constan {
 
     companion object{
-        val BASE_URL = "http://192.168.43.63/upload_photo/"
+        val BASE_URL = "http://192.168.0.24/uploadgambarkelocal/upload_fix/"
         object code {
             const val CAMERA_CODE = 0
             const val GALLERY_CODE = 1
